ec2b877ac2f10f10cb1522c081993b57c3a01d69
Kono Steve <stevekono77@gmail.com>
2023-07-24 18:39:41 +0100

    Change title of the tab to 'Workout Pal'

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,24 +3,15 @@ import WorkoutDetails from '../components/WorkoutDetails';
 import { useLoaderData } from 'react-router-dom';
 
 export default function Home() {
-  
+
   // const rawWorkouts = useLoaderData();
   // console.log("Workouts from loader: ",rawWorkouts);
 
   const [workouts,setWorkouts] = useState(useLoaderData());
 
-  // useEffect(() => {
-  //   const fetchWorkouts = async () => {
-  //     const response = await fetch('http://localhost:4000/api/workouts');
-  //     const json = await response.json();
-
-  //     if(response.ok) {
-  //       setWorkouts(json);
-  //     }
-  //   }
-
-  //   fetchWorkouts();
-  // },[]);
+  useEffect(() => {
+    document.title = 'Workout Pal!';
+  },[]);
 
 
   return (
