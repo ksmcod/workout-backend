44ce7d9f9703e8088acc598c952d3e62b23a9e9b
Kono Steve <stevekono77@gmail.com>
2023-07-29 03:45:24 +0100

    Created a hook that provides the workout context when invoqued

--- src/context/WorkoutContext.jsx
+++ src/context/WorkoutContext.jsx
@@ -3,10 +3,26 @@ import { createContext } from "react";
 
 export const WorkoutContext = createContext();
 
+export function workoutsReducer(state, action) {
+  switch (action.type) {
+    case 'SET_WORKOUTS':
+      return { workouts: action.payload };
+    case  'CREATE_WORKOUT':
+      return { workouts: [action.payload, ...state]};
+    default:
+      return state;
+  }
+}
+
 
 export default function WorkoutContextProider({ children }) {
+
+  const [state,dispatch] = useReducer(workoutsReducer,{
+    workouts:null
+  });
+
   return (
-    <WorkoutContext.Provider value={{}}>
+    <WorkoutContext.Provider value={{state,dispatch}}>
         { children }
     </WorkoutContext.Provider>
   )
