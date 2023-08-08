9684d08db3f369ed8e9ee358f89093441a266b40
Kono Steve <stevekono77@gmail.com>
2023-08-08 22:24:24 +0100

    Formatted added date of the workout usin date-fns

--- src/components/WorkoutDetails.jsx
+++ src/components/WorkoutDetails.jsx
@@ -1,5 +1,6 @@
 import useWorkoutsContext  from '../hooks/useWorkoutsContext'
 import trash from '../assets/trash.svg'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
 
 export default function WorkoutDetails({ workout }) {
@@ -21,9 +22,9 @@ export default function WorkoutDetails({ workout }) {
         <h4 className='mb-3 text-xl font-bold text-emerald-500'>{workout.title}</h4>
         <p className='m-0 text-sm text-gray-600'><span className="font-bold">Load (kg): </span>{workout.load}</p>
         <p className='m-0 text-sm text-gray-600'><span className="font-bold">Reps: </span>{workout.reps}</p>
-        <p className='m-0 text-sm text-gray-600'>{workout.createdAt}</p>
+        <p className='m-0 text-sm text-gray-600'>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix:true })}</p>
 
-        <span className="absolute right-2 top-2 cursor-pointer shadow-lg rounded-full" onClick={handleDelete}><img src={trash} className='w-7' /></span>
+        <span className="absolute right-2 top-2 cursor-pointer shadow-lg rounded-full p-1" onClick={handleDelete}><img src={trash} className='w-7' /></span>
     </div>
   )
 }
