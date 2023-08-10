06d5ac74c9696513e67387c24abfd5c6e23a9fbf
Kono Steve <stevekono77@gmail.com>
2023-08-10 17:13:37 +0100

    Implemented state logic in signup form to hold state of email and password to later be used for POST request

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
 
 export default function Signup() {
+  
+  const [state,setState] = useState({ email:'', password:'' });
+
+  function formHandler(e) {
+    if (e.target == form) {
+      e.preventDefault();
+      console.log("Final State: ",state)
+    }
+
+    if(e.target.name == 'email') {
+      setState(prevState => ({ ...prevState, email:e.target.value }));
+    }
+    if(e.target.name == 'password') {
+      setState(prevState => ({ ...prevState, password:e.target.value }));
+    }
+  }
+
   return (
-<form name='form' onSubmit={e => formHandler(e)} className='sm:space-y-4 space-y-2 sm:p-1 p-10 max-w-sm mx-auto mt-10'>
+<form name='form' onSubmit={e => formHandler(e)} className='sm:space-y-4 space-y-2 sm:p-14 p-10 max-w-sm mx-auto mt-10 rounded bg-white'>
 
         <h3 className='font-bold text-4xl text-center'>Sign Up</h3>
 
@@ -9,14 +27,16 @@ export default function Signup() {
             <label htmlFor="email" className='font-semibold'>Email</label>
             <input
              className='px-2 py-1 w-full border border-gray-100 rounded-md focus:outline-none shadow'
-             type="text" required name='email' id="email"/>
+             type="text" required name='email' id="email" onChange={e => formHandler(e)}
+            value={state.email}/>
         </div>
 
         <div className="space-y-1">
             <label htmlFor="password" className='font-semibold'>Password</label>
             <input
              className='px-2 py-1 w-full border border-gray-100 rounded-md focus:outline-none shadow'
-             type="password" required name='password' id="password" />
+             type="password" required name='password' id="password" onChange={e => formHandler(e)} 
+             value={state.password}/>
         </div>
 
         <button className='bg-emerald-500 border-0 text-white py-2 rounded cursor-pointer w-full'>Sign Up</button>
